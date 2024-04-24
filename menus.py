811c0d1082524f260689975ce3707e24bce7e3ca
Derguene <derguene.mbaye@baamtu.com>
2024-04-24 15:56:31 +0000

    Externalizing menus used in custom actions

---
+++ menus.py
@@ -0,0 +1,17 @@
+main = """\
+Choose your favorite Deep Learning Framework:\n
+1. PyTorch 🔥\n
+2. TensorFlow 🌊\n
+3. DeepLearning4J ☕️\n
+4. CNTK 🧠\n
+5. Keras 🔗\n
+6. ONNX 🌐\n
+7. MXNET 🎛️\n
+8. Caffe ⚡️\n
+"""
+
+pytorch_version = """\
+What version of PyTorch do you want to explore ?\n
+1. Version 0.x\n
+2. Version 1.x
+"""
